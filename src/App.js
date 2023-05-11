cb4be739b5578e875928ead0f6a9f9772e3b6f6f
Memo Diaz <121352998+memodiaz44@users.noreply.github.com>
2023-05-11 14:56:35 -0600

    updated commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from "./image/logo.JPG"
+import Button from './components/Button';
+import Screen from './components/Screen';
+import Clear from './components/Clear';
+import { useState } from 'react';
+import { evaluate } from 'mathjs' 
 
 function App() {
+  
+const [input, setInput] = useState ('')
+
+const addInput = val => {
+  setInput(input + val)
+};
+
+const calcilateResult = () => {
+  if (input) { 
+  setInput(evaluate(input))
+   }else {
+    alert('please enter a value')
+   }
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='my-logo-conteiner'>
+        <img
+        src={Logo}
+        className="my-logo"
+        alt='my logo'  />
+      </div>
+      <div 
+      className='calculator-conteiner'>
+        <Screen input={input}/>
+        
+        <div className='fila'>
+        <Button manageClick={addInput}>1</Button>
+        <Button manageClick={addInput}>2</Button>
+        <Button manageClick={addInput}>3</Button>
+        <Button manageClick={addInput}>+</Button>
+        </div>
+        <div className='fila'>
+        <Button manageClick={addInput}>4</Button>
+        <Button manageClick={addInput}>5</Button>
+        <Button manageClick={addInput}>6</Button>
+        <Button manageClick={addInput}>-</Button>
+        </div>
+        <div className='fila'>
+        <Button manageClick={addInput}>7</Button>
+        <Button manageClick={addInput}>8</Button>
+        <Button manageClick={addInput}>9</Button>
+        <Button manageClick={addInput}>*</Button>
+        </div>
+        <div className='fila'>
+        <Button manageClick={calcilateResult}>=</Button>
+        <Button manageClick={addInput}>0</Button>
+        <Button manageClick={addInput}>.</Button>
+        <Button manageClick={addInput}>/</Button>
+        </div>
+        <div className='fila'>
+          <Clear manageClick={() => setInput('')}>Clear</Clear> 
+          </div>
+        
+
+      </div>
+
+   
     </div>
   );
 }
